3dad0b5e97787faaa88e62fae8a34d7ca3142f99
Ludovic Estevenet <lestevenet@exomind.fr>
2022-10-19 14:23:25 +0200

    [FIX] Appdelegate

--- Weather/App/AppDelegate.swift
+++ Weather/App/AppDelegate.swift
@@ -10,14 +10,10 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-	var window:UIWindow?
+	var window: UIWindow?
 	
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-		self.window = UIWindow(frame: UIScreen.main.bounds)
-		window?.rootViewController = UIStoryboard(name: "Weather", bundle: nil).instantiateInitialViewController()
-		window?.makeKeyAndVisible()
-		
-		return true
+		true
 	}
 }
 
