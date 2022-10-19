658a3547c73a16b410fcb62af8547b82c5c23eda
Ludovic Estevenet <lestevenet@exomind.fr>
2022-10-19 17:22:38 +0200

    [Response] Fix codingKeys

--- DataKit/Data/Response/WeatherCityResponse.swift
+++ DataKit/Data/Response/WeatherCityResponse.swift
@@ -17,12 +17,26 @@ public struct WeatherCityResponse: Codable {
 
 	// MARK: - Main
 	public struct Main: Codable {
+		
+		enum CodingKeys: String, CodingKey {
+			case temp, pressure, humidity,
+				 feelsLike = "feels_like",
+				 tempMin = "temp_min",
+				 tempMax = "temp_max"
+		}
+		
 		public let temp, feelsLike, tempMin, tempMax: Double
 		public let pressure, humidity: Int
 	}
 
 	// MARK: - Weather
 	public struct Weather: Codable {
+		
+		enum CodingKeys: String, CodingKey {
+			case id, main, icon,
+				 weatherDescription = "description"
+		}
+		
 		public let id: Int
 		public let main, weatherDescription, icon: String
 	}
